2bae73dbb9eb366d5df436afef662f057f6e9678
青木秀仁 <shamrock1976@me.com>
2022-09-04 22:27:28 +0900

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,141 @@
 # udtalk-apn
+
+UDトークのプッシュ通知のサーバー側の実装です。
+
+Web APIは4つ
+
+registerDevice
+unregisterDevice
+updateDeviceStatus
+pushNewUtteranceNotification
+deleteUnusedDevices（※メンテナンス用）
+
+アプリでトークに参加したタイミングでregisterDeviceを実行します。その後、トークに参加して画面を見ている間はupdateDeviceStatusを1分ごとに呼び出しtimestampを更新します。トークから退出したらunregisterDeviceを実行します。
+
+pushNewUtteranceNotificationは音声認識の結果が生成されるたびに呼び出されます。そのときに
+
+- 最後のupdateDeviceStatusから1分30秒が経過しているときはプッシュ通知を送る
+- 以前にプッシュ通知を送ってから15分経過してまだupdateDeviceStatusが呼ばれてなければプッシュ通知を送る
+- updateDeviceStatusが最後に呼ばれてから2時間が経過していたらプッシュ通知は以降送らない
+
+これらの条件でプッシュ通知が同じトークに参加している端末に送信されます。
+
+アプリが途中で落ちたり、強制終了した場合などはトークの退出が行われないケースがあるので、数日に一度deleteUnusedDevicesを呼び出しで必要のなくなったデータは削除します。
+
+## 動かし方
+1. Node.jsをインストールします
+
+2. npm install
+
+展開後のフォルダで実行します。
+
+3. .envを作成してプロジェクト直下に配置
+
+※_envファイルは雛形ですのでご利用ください
+
+以下の内容をコピーしてプロジェクトのルートフォルダに.envファイルを作成してください。
+
+```
+ROOT_URL = "http://localhost:3000"
+
+FIREBASE_API_KEY = ""
+FIREBASE_AUTH_DOMAIN = ""
+FIREBASE_PROJECT_ID = ""
+FIREBASE_STORAGE_BUCKET = ""
+FIREBASE_MESSAGING_SENDER_ID = ""
+FIREBASE_APP_ID = ""
+MEASUREMENT_ID = ""
+
+GOOGLE_CLOUD_TRANSLATION_API_KEY = ""
+
+FIREBASE_ADMINSDK_type = ""
+FIREBASE_ADMINSDK_project_id = ""
+FIREBASE_ADMINSDK_private_key_id = ""
+FIREBASE_ADMINSDK_private_key = ""
+FIREBASE_ADMINSDK_client_email = ""
+FIREBASE_ADMINSDK_client_id = ""
+FIREBASE_ADMINSDK_auth_uri = ""
+FIREBASE_ADMINSDK_token_uri = ""
+FIREBASE_ADMINSDK_auth_provider_x509_cert_url = ""
+FIREBASE_ADMINSDK_client_x509_cert_url = ""
+```
+### firestoreへのアクセス
+
+firestoreへのアクセス用に発行されたコードを環境変数に移してください。
+
+```
+var firebaseConfig = {
+    apiKey: "xxxxx,
+    authDomain: "xxxxx",
+    projectId: "xxxxx",
+    storageBucket: "xxxxx",
+    messagingSenderId: "xxxxx",
+    appId: "xxxxx",
+    measurementId: "xxxxx"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+```
+このコードのxxxxxを
+```
+FIREBASE_API_KEY = "xxxxx"
+FIREBASE_AUTH_DOMAIN = "xxxxx"
+FIREBASE_PROJECT_ID = "xxxxx"
+FIREBASE_STORAGE_BUCKET = "xxxxx"
+FIREBASE_MESSAGING_SENDER_ID = "xxxxx"
+FIREBASE_APP_ID = "xxxxx"
+MEASUREMENT_ID = "xxxxx"
+```
+このように。
+
+ローカルで動作させるとfirestoreのインデックスを作成するアラートがでるのでエラーメッセージのURLから作成してください。
+
+### firebase adminへのアクセス
+
+firebase adminはサービス アカウントで発行した秘密鍵情報のjsonファイルをダウンロードして入力してください。
+AuthenticationのSign-in methodで「メール／パスワード」を有効にしてください。
+
+```
+FIREBASE_ADMINSDK_type = ""
+FIREBASE_ADMINSDK_project_id = ""
+FIREBASE_ADMINSDK_private_key_id = ""
+FIREBASE_ADMINSDK_private_key = ""
+FIREBASE_ADMINSDK_client_email = ""
+FIREBASE_ADMINSDK_client_id = ""
+FIREBASE_ADMINSDK_auth_uri = ""
+FIREBASE_ADMINSDK_token_uri = ""
+FIREBASE_ADMINSDK_auth_provider_x509_cert_url = ""
+FIREBASE_ADMINSDK_client_x509_cert_url = ""
+```
+
+### 動作させるドメイン
+
+動作させるドメインを入力してください。
+
+`ROOT_URL = "http://localhost:3000"`
+
+Heroku等で動かす場合はこれらをインスタンスの環境編集に登録してください。認証情報になりますので、公開リポジトリには含めないように注意してください。
+なおHerokuのインスタンスに環境変数としてFIREBASE_ADMINSDK_private_keyをセットする時は\nを実際の改行に置換してセットしてください。
+
+### Appleのプッシュ通知
+
+```
+APPLE_APNS_AUTH_KEY = ""
+APPLE_KEY_ID = ""
+APPLE_TEAM_ID = ""
+APPLE_IOS_APP_BUNDLE_ID = ""
+APPLE_WATCHOS_APP_BUNDLE_ID = ""
+```
+
+node-apnで必要な情報です。各自の開発環境から設定してください。
+
+### その他
+
+```
+API_KEY = ""
+```
+
+Web APIのアクセスに必要なキーです。実際の環境では乱数でキーを生成して設定しています。
+
---
+++ routes/api.js
@@ -0,0 +1,421 @@
+var express = require('express');
+let admin = require('firebase-admin');
+var router = express.Router() ;
+var apn = require('apn');
+
+const wrap = fn => (...args) => fn(...args).catch(args[2]) ;
+
+// registerDevice
+router.post('/registerDevice', wrap(async function(req, res, next) {
+    let key = req.body["key"] ;
+
+    if (key != process.env.API_KEY) {
+        let result = {} ;
+
+        result["result"] = false ;
+    
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(result));	
+
+        return ;
+    }
+
+    let userId = req.body["userId"] ;
+    let talkId = req.body["talkId"] ;
+    let deviceToken = req.body["deviceToken"] ;
+
+    req.body["timestamp"] = Date.now() ;
+    req.body["lastPublishTimestamp"] = 0 ;
+
+    if (deviceToken != "" && deviceToken != undefined) {
+        let talkDoc = admin.firestore().collection("talks").doc(talkId) ;
+
+        let talkSnapshot = await talkDoc.get() ;
+        let talkData = talkSnapshot.data() ;
+
+        if (talkData == null) {
+            await talkDoc.set({"userCount" : "1"}) ;
+            await await talkDoc.collection("users").doc(userId).set(req.body) ;
+        } else {
+            let userDoc = talkDoc.collection("users").doc(userId) ;
+
+            let userSnapshot = await userDoc.get() ;
+            let userData = userSnapshot.data() ;
+
+            if (userData == null) {
+                let userCount = Number(talkData["userCount"]) ;
+
+                userCount++ ;
+
+                await talkDoc.set({"userCount" :userCount}) ;
+            }
+                
+            await userDoc.set(req.body) ;
+        }
+    }
+
+    let result = {} ;
+
+    result["result"] = true ;
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));	
+})) ;
+
+// unregisterDevice
+router.post('/unregisterDevice', wrap(async function(req, res, next) {
+    let key = req.body["key"] ;
+
+    if (key != process.env.API_KEY) {
+        let result = {} ;
+
+        result["result"] = false ;
+    
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(result));	
+
+        return ;
+    }
+
+    let userId = req.body["userId"] ;
+    let talkId = req.body["talkId"] ;
+    
+    let talkDoc = admin.firestore().collection("talks").doc(talkId) ;
+    let userDoc = talkDoc.collection("users").doc(userId) ;
+
+    let userSnapshot = await userDoc.get() ;
+    let userData = userSnapshot.data() ;
+
+    if (userData != null) {
+        await userDoc.delete() ;
+
+        let talkSnapshot = await talkDoc.get() ;
+        let talkData = talkSnapshot.data() ;
+        let userCount = Number(talkData["userCount"]) ;
+
+        userCount--;
+
+        if (userCount == 0) {
+            await talkDoc.delete() ;
+        } else {
+            await talkDoc.set({"userCount" :userCount}) ;
+        }
+    }
+
+    let result = {} ;
+
+    result["result"] = true ;
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));	
+})) ;
+
+// updateDeviceStatus
+router.post('/updateDeviceStatus', wrap(async function(req, res, next) {
+    let key = req.body["key"] ;
+
+    if (key != process.env.API_KEY) {
+        let result = {} ;
+
+        result["result"] = false ;
+    
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(result));	
+
+        return ;
+    }
+
+    let userId = req.body["userId"] ;
+    let talkId = req.body["talkId"] ;
+    
+    req.body["timestamp"] = Date.now() ;
+
+    let talkDoc = admin.firestore().collection("talks").doc(talkId) ;
+    let userDoc = talkDoc.collection("users").doc(userId) ;
+
+    let userSnapshot = await userDoc.get() ;
+    let userData = userSnapshot.data() ;
+
+    if (userData != null) {
+        await userDoc.update(req.body) ;
+    }
+
+    let result = {} ;
+
+    result["result"] = true ;
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));	
+})) ;
+
+// pushNewUtteranceNotification
+router.post('/pushNewUtteranceNotification', wrap(async function(req, res, next) {
+    let key = req.body["key"] ;
+
+    if (key != process.env.API_KEY) {
+        let result = {} ;
+
+        result["result"] = false ;
+    
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(result));	
+
+        return ;
+    }
+
+    let userId = req.body["userId"] ;
+    let talkId = req.body["talkId"] ;
+    let forcePublishing = req.body["forcePublishing"] ;
+
+    let talkDoc = admin.firestore().collection("talks").doc(talkId) ;
+
+    let timestamp = Date.now() ;
+    let usersDoc ;
+
+    if (forcePublishing == "1") {
+        usersDoc = talkDoc.collection("users") ;
+    } else {
+        usersDoc = talkDoc.collection("users").where('timestamp', '<=', timestamp - 90 * 1000).where('timestamp', '>=', timestamp - 120 * 60 * 1000) ;
+    }
+
+    let usersSnapshot = await usersDoc.get() ;
+    
+    usersSnapshot.forEach(userSnapshot => {
+        let userData = userSnapshot.data() ;
+
+        if (userData["userId"] != userId && 
+            (userData["lastPublishTimestamp"] <= timestamp - 15 * 60 * 1000 || forcePublishing == "1")) { 
+
+            if (userData["type"] == "iOS" || userData["type"] == "watchOS" || userData["type"] == "watchOS_via_iOS") {
+                
+                let bundleId = "" ;
+                
+                if (userData["type"] == "iOS" || userData["type"] == "watchOS_via_iOS") {
+                    bundleId = process.env.APPLE_IOS_APP_BUNDLE_ID ;
+                } else if (userData["type"] == "watchOS") {
+                    bundleId = process.env.APPLE_WATCHOS_APP_BUNDLE_ID ;
+                }
+
+                sendPushNotification(
+                    userData["deviceToken"], 
+                    userData["env"] == "pro", 
+                    userData["languageCode"],
+                    bundleId,
+                    userData["type"]) ;
+
+                if (forcePublishing != "1") {
+                    userData["lastPublishTimestamp"] = timestamp ;
+                }
+
+                let userDoc = talkDoc.collection("users").doc(userData["userId"]) ;
+
+                userDoc.update(userData) ;
+            } else if (userData["type"] == "Android") {
+                sendFCM(
+                    userData["deviceToken"], 
+                    userData["languageCode"]) ;
+
+                if (forcePublishing != "1") {
+                    userData["lastPublishTimestamp"] = timestamp ;
+                }
+
+                let userDoc = talkDoc.collection("users").doc(userData["userId"]) ;
+
+                userDoc.update(userData) ;
+            }
+        }
+    }); 
+
+    let result = {} ;
+
+    result["result"] = true ;
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));	
+})) ;
+
+// deleteUnusedDevices
+router.post('/deleteUnusedDevices', wrap(async function(req, res, next) {
+    let key = req.body["key"] ;
+
+    if (key != process.env.API_KEY) {
+        let result = {} ;
+
+        result["result"] = false ;
+    
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(result));	
+
+        return ;
+    }
+
+    let timestamp = Date.now() ;
+
+    let talksDoc = admin.firestore().collection("talks")
+
+    let talksSnapshot = await talksDoc.get() ;
+    
+    talksSnapshot.forEach(async talkSnapshot => {
+      
+        let talkData = talkSnapshot.data() ;
+
+        const prevUserCount = Number(talkData["userCount"]) ;
+        
+        let userCount = prevUserCount ;
+
+        let talkDoc = await talksDoc.doc(talkSnapshot.id) ;
+        let usersDoc = talkDoc.collection("users") ;
+
+        let usersSnapshot = await usersDoc.get() ;
+    
+        usersSnapshot.forEach(async userSnapshot => {
+            let userData = userSnapshot.data() ;
+
+            if ((timestamp - 120 * 60 * 1000) > userData["timestamp"]) {
+                userCount--;
+                let userDoc = await usersDoc.doc(userSnapshot.id) ;
+                await userDoc.delete() ;
+            }
+        }) ;
+
+        if (userCount == 0) {
+            console.log("delete this talkId : " + talkSnapshot.id) ;
+            await talkDoc.delete() ;
+        } else {
+            if (userCount != prevUserCount) {
+                console.log("update this talkId : " + talkSnapshot.id) ;
+                await talkDoc.set({"userCount" :userCount}) ;
+            }
+        }
+    }) ;
+
+    let result = {} ;
+
+    result["result"] = true ;
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));	
+})) ;
+
+// sendPushNotification
+function sendPushNotification(
+    deviceToken, 
+    production, 
+    languageCode,
+    bundleId,
+    type
+    ) {
+    var options = {
+        token: {
+          key: process.env.APPLE_APNS_AUTH_KEY,
+          keyId: process.env.APPLE_KEY_ID,
+          teamId: process.env.APPLE_TEAM_ID,
+        },
+        production: production
+    };
+
+    var apnProvider = new apn.Provider(options);
+
+    var note = new apn.Notification();
+    
+    apn.Notification.prototype.headers = function headers() {
+        let headers = {};
+        
+        if (this.priority !== 10) {
+            headers["apns-priority"] = this.priority;
+        }
+        
+        if (this.id) {
+            headers["apns-id"] = this.id;
+        }
+        
+        if (this.expiry >= 0) {
+            headers["apns-expiration"] = this.expiry;
+        }
+        
+        if (this.topic) {
+            headers["apns-topic"] = this.topic;
+        }
+
+        if (this.pushType) {
+            headers["apns-push-type"] = this.pushType
+        }
+        
+        if (this.collapseId) {
+            headers["apns-collapse-id"] = this.collapseId;
+        }
+        
+        return headers;
+    };
+    
+    let message ;
+
+    if (languageCode.startsWith("ja-")) {
+        if (type == "iOS") {
+            message = "" ;
+        } else if (type == "watchOS" || type == "watchOS_via_iOS") {
+            message = "Apple Watchで" ;
+        } else {
+            message = "" ;
+        }
+
+        message += "参加しているトークに新しい発話がありました。" ;
+    } else {
+        message = "Your joined talks had new messages" ;
+
+        if (type == "iOS") {
+            message = "." ;
+        } else if (type == "watchOS" || type == "watchOS_via_iOS") {
+            message = " on Apple Watch." ;
+        } else {
+            message = "." ;
+        }
+    }
+    
+    note.badge = 0;
+    note.body = message ;
+    note.topic = bundleId;
+    note.sound = "ping.aiff";
+    note.pushType = "alert" ;
+    note.contentAvailable = true;
+    note.priority = 5;
+
+    apnProvider.send(note, deviceToken).then( (result) => {
+        console.log(bundleId + " : " + deviceToken) ;
+        console.log(result) ;
+    });
+}
+
+// sendFCM
+function sendFCM(
+    deviceToken, 
+    languageCode) 
+{
+
+    let title = "" ;
+    let body = "" ;
+
+    if (languageCode.startsWith("ja-")) {
+        title = "UDトーク" ;
+        body = "参加しているトークに新しい発話がありました。" ;
+    } else {
+        title = "UDTalk" ;
+        body = "Your joined talks had new messages" ;
+    }
+
+    const message = {
+        notification: {
+            title: title,
+            body: body
+        },
+        token: deviceToken
+    };
+    
+    admin.messaging().send(message).then((response) => {
+        console.log('Successfully sent message:', response);
+    }).catch((error) => {
+        console.log('Error sending message:', error);
+    });
+}
+
+module.exports = router;
